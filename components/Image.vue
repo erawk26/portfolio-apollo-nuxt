e115a417b3b9d1391b43a160cf29205c6f492402
Erik Olsen <erawk26@gmail.com>
2020-01-12 12:43:18 -0500

    image component tweak to use vuetify and theme image style better

--- components/Image.vue
+++ components/Image.vue
@@ -1,10 +1,13 @@
 <template lang="pug">
-  img(:alt="alt" :src="`https://media.graphcms.com/resize=w:${imgWidth},h:${imgHeight},fit:crop/${handle}`")
+  v-img(:alt="alt" :lazy-src="`http://via.placeholder.com/${imgWidth}x${imgHeight}`" :src="imageUrl")
+    template(v-slot:placeholder)
+      v-row.fill-height.ma-0(align='center' justify='center')
+        v-progress-circular(indeterminate color='grey lighten-5')
 </template>
 
 <script>
 export default {
-  data: () => ({imgWidth: 100, imgHeight: 100}),
+  data: () => ({resize: null, imgWidth: 100, imgHeight: 100, imageUrl: ''}),
   props: {
     imageStyle: { type: String, default: 'thumb' },
     handle: { type: String, default: '' },
@@ -16,17 +19,24 @@ export default {
       case 'thumb':
         this.imgWidth = 100
         this.imgHeight = 100
+        this.resize = `resize=w:${this.imgWidth},h:${this.imgHeight},fit:crop/`
         break
       case 'full':
         this.imgWidth = 1600
         this.imgHeight = 900
+        this.resize = `resize=w:${this.imgWidth},h:${this.imgHeight},fit:crop/`
         break
       case 'avatar':
         this.imgWidth = 300
         this.imgHeight = 300
+        this.resize = `resize=w:${this.imgWidth},h:${this.imgHeight},fit:crop/`
         break
+      case 'original':
+        this.resize = ''
     }
+    this.imgUrl = `https://media.graphcms.com/${this.resize}${this.handle}`
   },
+  methods: {},
   computed: {
   }
 }
