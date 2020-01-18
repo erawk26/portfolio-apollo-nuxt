1e5f6f1d7874e68cc3b3278f84568334763440eb
Erik Olsen <erawk26@gmail.com>
2020-01-18 12:42:37 -0500

    moved apollo project and skills queries to a mixin

--- plugins/methods.js
+++ plugins/methods.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
+import G from '~/graphql/gql.js'
 Vue.mixin({
   methods: {
     machine_readable: (val) => val
       .toLowerCase()
       .replace(/[^\w ]+/g, '')
       .replace(/ +/g, '-')
+  },
+  apollo: {
+    $loadingKey: 'loading',
+    skills: {
+      query: G.queries.skills
+    },
+    projects: {
+      query: G.queries.projects
+    }
   }
 })
