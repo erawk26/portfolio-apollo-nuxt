d0eb0d34a2fc739a6cdadbd0a553a426280af3bb
Erik Olsen <erawk26@gmail.com>
2020-05-16 10:04:37 -0400

    change vuetify and eslint config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,72 +1,69 @@
 module.exports = {
   head: {
-    title: 'nuxt-portfolio',
+    title: "nuxt-portfolio",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'GraphCMS Blog starter using Apollo Client and Nuxt.js' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "GraphCMS Blog starter using Apollo Client and Nuxt.js",
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' },
+      { rel: "icon", type: "image/x-icon", href: "./favicon.ico" },
       {
-        rel: 'stylesheet',
+        rel: "stylesheet",
         href:
-          'https://cdnjs.cloudflare.com/ajax/libs/hamburgers/1.1.3/hamburgers.min.css'
+          "https://cdnjs.cloudflare.com/ajax/libs/hamburgers/1.1.3/hamburgers.min.css",
       },
       {
-        rel: 'stylesheet',
+        rel: "stylesheet",
         href:
-          'https://fonts.googleapis.com/css?family=Lato|Unica+One|Material+Icons'
-      }
-    ]
+          "https://fonts.googleapis.com/css?family=Lato|Unica+One|Material+Icons",
+      },
+    ],
   },
   build: {
-    extend (config, ctx) {
-      if (ctx.dev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    },
+    extend(config, ctx) {},
     postcss: {
       plugins: {
-        'postcss-import': true
+        "postcss-import": true,
       },
       preset: {
         // Change the postcss-preset-env settings
         autoprefixer: {
-          grid: true
-        }
-      }
-    }
+          grid: true,
+        },
+      },
+    },
   },
-  modules: [
-    '@nuxtjs/apollo',
-    '@nuxtjs/vuetify',
-    '@nuxtjs/google-analytics',
-    '@nuxtjs/style-resources'
+  modules: [],
+  buildModules: [
+    "@nuxtjs/eslint-module",
+    "@nuxtjs/apollo",
+    "@nuxtjs/vuetify",
+    "@nuxtjs/google-analytics",
+    "@nuxtjs/style-resources",
   ],
   googleAnalytics: {
-    id: 'G-7T0JLV856W'
+    id: "G-7T0JLV856W",
   },
   styleResources: {
     // your settings here
-    scss: ['~/assets/scss/_init.scss'] // alternative: scss
+    scss: ["~/assets/scss/_init.scss"], // alternative: scss
   },
   plugins: [
-    '~/plugins/methods.js'
+    "~/plugins/methods.js",
     // '~/plugins/vuetify.js'
   ],
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    optionsPath: './vuetify.options.js'
+    customVariables: ["~/assets/scss/_vuetify.scss"],
+    optionsPath: "./vuetify.options.js",
   },
   apollo: {
     clientConfigs: {
-      default: '~/apollo/client-configs/default.js'
-    }
-  }
-}
+      default: "~/apollo/client-configs/default.js",
+    },
+  },
+};
